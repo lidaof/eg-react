3c6ece538cd781a69e09e7741cd54c39ed6f47da
Daofeng Li <lidaof@gmail.com>
2018-02-02 16:32:30 -0600

    cytoband

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017'
 const dbName = 'hg19';
 
+const GENE_SEARCH_LIMIT = 20;
+
 const server = new Hapi.Server();
 
-server.connection({port:3001, host: 'localhost'});
+server.connection({port:3001, host: '0.0.0.0'});
 
 server.route({
     method: 'GET',
@@ -30,19 +32,21 @@ server.route({
 
 function partialRefGeneSearch(q, db, callback){
     let collection = db.collection('refGene');
-    //let query = {$or: [ {name: `/^${q}/i`}, {name2: `/^${q}/i`} ] };
-    let query = {name2: `/^${q}/i`}
+    let query = {$or: [ {name: { $regex: `^${q}`, $options: 'i' } }, {name2: { $regex: `^${q}`, $options: 'i' } } ] };
+    //let query = {name2: { $regex: `^${q}`, $options: 'i' } };
     console.log(query);
-    let cursor = collection.find(query);
+    let cursor = collection.find(query).limit(GENE_SEARCH_LIMIT);
+    let res = [];
     cursor.each( (err, doc) => {
         assert.equal(err, null);
         if (doc != null){
-            console.log(doc);
-            return doc;
+            //console.log(doc);
+            res.push(doc);
         }else{
-            callback();
+            callback(res);
         }
     });
+    //return res;
 }
 
 server.route({
@@ -54,10 +58,47 @@ server.route({
             const db = client.db(dbName);
             let que = encodeURIComponent(request.params.q);
             console.log(que);
-            let res = partialRefGeneSearch(que, db, () => {
+            partialRefGeneSearch(que, db, (res) => {
+                //console.log(res);
+                reply(res).header('content-type','application/json');
                 client.close();
-            });
-            reply(res);
+            });  
+        });
+    }
+});
+
+function cytoBandSearch(q, db, callback){
+    let collection = db.collection('cytoBand');
+    let query = {chrom:q};
+    console.log(query);
+    let cursor = collection.find(query);
+    let res = [];
+    cursor.each( (err, doc) => {
+        assert.equal(err, null);
+        if (doc != null){
+            //console.log(doc);
+            res.push(doc);
+        }else{
+            callback(res);
+        }
+    });
+    //return res;
+}
+
+server.route({
+    method: 'GET',
+    path:'/hg19/cytoBand/{q}',
+    handler: function(request, reply){
+        MongoClient.connect(url, (err, client) => {
+            assert.equal(null, err);
+            const db = client.db(dbName);
+            let que = encodeURIComponent(request.params.q);
+            console.log(que);
+            cytoBandSearch(que, db, (res) => {
+                //console.log(res);
+                reply(res).header('content-type','application/json');
+                client.close();
+            });  
         });
     }
 });
