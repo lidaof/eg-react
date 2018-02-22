26147a6d641936db08abe2d5ea109ce90e17c1f8
Daofeng Li <lidaof@gmail.com>
2018-02-22 16:38:12 -0600

    adding gene parse

--- v1/src/model/refGene.js
+++ v1/src/model/refGene.js
@@ -1,47 +1,52 @@
+import React from 'react';
 import Feature from './Feature';
 import ChromosomeInterval from './interval/ChromosomeInterval';
-import JSON5 from 'json5';
-const validate = require('jsonschema').validate;
-
-/**
- * Makes sure that the input the `details` part of Gene object is well-formed
- * 
- * @param {object} obj - object to validate
- * @return {ValidationError[]} - array of validation errors, which will be empty if there were no errors.
- */
-function validateGene(obj) { // eslint-disable-line no-unused-vars
-    let validationResult = validate(obj, DETAILS_SCHEMA);
-    return validationResult.errors;
-}
-
-const DETAILS_SCHEMA = { // Only used in validateGene, but it happens to serve as good documentation too.
-    name: {type: "string"}, // More like ID
-    name2: {type: "string"}, // Actual gene name
-    strand: {type: "string"},
-    id: {type: "integer"},
-    desc: {type: "string"},
-    struct: {
-        thick: {type: "array"},
-        thin: {type: "array"}
-    }
-}
+import _ from 'lodash';
 
 /**
  * A data container for gene annotations originating from hammock files.
  * 
- * @author Silas Hsu
+ * @author Daofeng Li, modified from Silas's Gene component
  */
+
+export class RefGeneRecord extends React.Component {
+    constructor(record){
+        super();
+        this.record = record;
+    }
+}
+
+
 export class Gene extends Feature {
     /**
-     * Constructs a new Gene, given a BedRecord from a hammock file.  The other parameters calculate absolute
+     * Constructs a new Gene, given an entry from mongodb.  The other parameters calculate absolute
      * coordinates.
-     * 
-     * @param {BedRecord} record - BedRecord-like object to use
+    {
+        "_id": "5a6a4edfc019c4d5b606c0e8",
+        "bin": 792,
+        "name": "NR_037940",
+        "chrom": "chr7",
+        "strand": "-",
+        "txStart": 27202056,
+        "txEnd": 27219880,
+        "cdsStart": 27219880,
+        "cdsEnd": 27219880,
+        "exonCount": 3,
+        "exonStarts": "27202056,27204496,27219264,",
+        "exonEnds": "27203460,27204586,27219880,",
+        "score": 0,
+        "name2": "HOXA10-HOXA9",
+        "cdsStartStat": "unk",
+        "cdsEndStat": "unk",
+        "exonFrames": "-1,-1,-1,"
+    }
+     * @param {RefGeneRecord} record - BedRecord-like object to use
      * @param {NavigationContext} navContext - used to calculate absolute coordinates
      * @param {FeatureInterval} featureInterval - a feature which overlaps this 
      */
-    constructor(bedRecord, navContext, featureInterval) {
-        const location = new ChromosomeInterval(bedRecord.chr, bedRecord.start, bedRecord.end);
+    constructor(refGeneRecord, navContext, featureInterval) {
+        
+        const location = new ChromosomeInterval(refGeneRecord.chr, refGeneRecord.txStart, refGeneRecord.txEnd);
         super(null, location);
 
         const absInterval = navContext.convertGenomeIntervalToBases(featureInterval, location);
@@ -52,23 +57,21 @@ export class Gene extends Feature {
         [this.absStart, this.absEnd] = absInterval;
         this._navContext = navContext;
         this._featureInterval = featureInterval;
-        this._unparsedDetails = bedRecord.details;
+        this.refGeneRecord = refGeneRecord;
     }
 
     /**
      * @inheritdoc
      */
     getName() {
-        const details = this.getDetails();
-        return details.name2 || details.name || "";
+        return this.refGeneRecord.name2 || this.refGeneRecord.name || "";
     }
 
     /**
      * @inheritdoc
      */
     getIsForwardStrand() {
-        const details = this.getDetails();
-        return details.strand !== "-";
+        return this.refGeneRecord.strand !== "-";
     }
 
     /**
@@ -79,8 +82,54 @@ export class Gene extends Feature {
      * @override
      */
     getDetails() {
+        const details = {};
+        const {cdsStart, cdsEnd, exonStarts, exonEnds} = this.refGeneRecord;
+        const exonStartList = _.trim(exonStarts,',').split(',');
+        const exonEndList = _.trim(exonEnds,',').split(',');
+        if(cdsStart === cdsEnd){
+            let tmp = [];
+            for (const [idx, pos] of exonStartList){
+                tmp.push([pos, exonEndList[idx]]);
+            }
+            details['thin'] = tmp;
+            return details;
+        }
+        const thick = [], thin = [];
+        for (const [idx, pos] of exonStartList){
+            let start = pos, end = exonEndList[idx];
+            if (end <= cdsStart){
+                thin.push([start, end]);
+            }else if(end <= cdsEnd){
+                if (start < cdsStart){
+                    thin.push([start, cdsStart]);
+                    thick.push([cdsStart, end]);
+                } else {
+                    thick.push([start, end]);
+                }
+            }else{
+                if(start < cdsStart){
+                    thin.push([start, cdsStart]);
+                    thin.push([cdsEnd, end]);
+                    thick.push([cdsStart, cdsEnd]);
+                }else{
+                    if (start < cdsEnd){
+                        thick.push([start, cdsEnd]);
+                        thin.push([cdsEnd, end]);
+                    }else{
+                        thin.push([start, end]);
+                    }
+                }
+            }
+        }
+        if (thin.length > 0){
+            details['thin'] = thin;
+        }
+        if (thick.length > 0){
+            details['thick'] = thick;
+        }
+        return details;
         if (!this._details) {
-            let details = JSON5.parse('{' + this._unparsedDetails + '}');
+            const details = this._details;
 
             // Set details.exons
             details.struct = details.struct || {};
@@ -118,7 +167,7 @@ export class GeneFormatter {
      * Turns BedRecords into Genes.  The second and third parameters exist to assist mapping to a navigation context.
      * Genes that fail mapping will be ignored and skipped.
      * 
-     * @param {BedRecord[]} bedRecords - the records to convert
+     * @param {BedRecord[]} refGeneRecords - the records to convert
      * @param {DisplayedRegionModel} region - object containing navigation context and view region
      * @param {FeatureInterval} feature - feature in navigation context to map to
      * @return {Gene[]} array of Gene
