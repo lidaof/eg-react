7ae50d2fc61a88d95f6b84c67bb5ee9b2a6d564d
Silas Hsu <hsu.silas@wustl.edu>
2018-06-05 11:23:15 -0500

    Added custom hub file upload
    Adding custom hub will also add tracks to view now
    Pull JSON5 fetching into separate class

--- frontend/src/components/trackManagers/CustomHubAdder.js
+++ frontend/src/components/trackManagers/CustomHubAdder.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import JSON5 from 'json5';
+import Json5Fetcher from '../../model/Json5Fetcher';
 import DataHubParser from '../../model/DataHubParser';
 
 class CustomHubAdder extends React.Component {
@@ -44,7 +46,52 @@ class CustomHubAdder extends React.Component {
     render() {
         return (
         <div>
-            <h4>Custom hub</h4>
+            <RemoteHubAdder onTracksAdded={this.props.onTracksAdded} />
+            <FileHubAdder onTracksAdded={this.props.onTracksAdded} />
+        </div>
+        );
+    }
+}
+
+class RemoteHubAdder extends React.Component {
+    static propTypes = {
+        onTracksAdded: PropTypes.func,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputUrl: ""
+        };
+        this.loadHub = this.loadHub.bind(this);
+    }
+
+    async loadHub() {
+        if (!this.props.onTracksAdded) {
+            return;
+        }
+
+        this.setState({isLoading: true});
+        let json;
+        try {
+            json = await new Json5Fetcher().get(this.state.inputUrl);
+        } catch (error) {
+            console.error(error);
+            this.setState({isLoading: false, error: "Error: HTTP " + error.status});
+            return;
+        }
+
+        const parser = new DataHubParser(0);
+        const tracks = await parser.getTracksInHub(json, "Custom hub");
+        if (tracks) {
+            this.props.onTracksAdded(tracks);
+            this.setState({isLoading: false, error: ""});
+        }
+    }
+
+    render() {
+        return (
+        <div>
             <label>
                 Custom hub URL
                 <input
@@ -54,11 +101,45 @@ class CustomHubAdder extends React.Component {
                 />
             </label>
             <button onClick={this.loadHub} disabled={this.state.isLoading || !this.state.inputUrl} >
-                Load custom hub
+                Load from URL
             </button>
             <p style={{color: "red"}} >{this.state.error}</p>
         </div>
         );
+    }
+}
+
+class FileHubAdder extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+    }
+
+    readFileAsText(file) {
+        const reader = new FileReader();
+        let promise = new Promise((resolve, reject) => {
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = () => reject(reader.error);
+        });
+        reader.readAsText(file);
+        return promise;
+    }
+
+    async handleFileUpload(event) {
+        if (!this.props.onTracksAdded) {
+            return;
+        }
+        const contents = await this.readFileAsText(event.target.files[0]);
+        const json = JSON5.parse(contents);
+        const parser = new DataHubParser(0);
+        const tracks = await parser.getTracksInHub(json, "Custom hub");
+        if (tracks) {
+            this.props.onTracksAdded(tracks);
+        }
+    }
+
+    render() {
+        return <div>Or <input type="file" onChange={this.handleFileUpload} /></div>;
     }
 }
 
--- frontend/src/model/DataHubParser.js
+++ frontend/src/model/DataHubParser.js
@@ -1,46 +1,25 @@
-import $ from 'jquery';
-import JSON5 from 'json5';
+import axios from 'axios';
 import TrackModel from './TrackModel';
 
 class HubParser {
-    constructor(tracksStartIndex=1) {
+    constructor(tracksStartIndex=0) {
         this.tracksStartIndex = tracksStartIndex;
+        this.samplesPromise = axios.get("/samples.json");
+        this.assaysPromise = axios.get("/assays.json");
     }
 
-    getJson(url) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: url,
-                dataType: "text", // Expected type of data from server
-            })
-            .done(data => {
-                try {
-                    resolve(JSON5.parse(data));
-                } catch (error) {
-                    reject(error);
-                }
-            })
-            .fail(reject);
-        });
-    }
-
-    async getTracksInHub(hubMetadata) {
-        let jsonPromises = Promise.all([
-            this.getJson(hubMetadata.url),
-            this.getJson("/samples.json"),
-            this.getJson("/assays.json"),
-        ]);
+    async getTracksInHub(parsedJson, hubName) {
+        let jsonPromises = Promise.all([this.samplesPromise, this.assaysPromise]);
         let jsons = await jsonPromises;
-        let hub = jsons[0];
-        let sampleDict = jsons[1];
-        let assayDict = jsons[2];
+        let sampleDict = jsons[0].data;
+        let assayDict = jsons[1].data;
 
         let tracks = [];
-        for (let plainObject of hub.slice(this.tracksStartIndex)) {
+        for (let plainObject of parsedJson.slice(this.tracksStartIndex)) {
             let newTrack = new TrackModel(plainObject);
             let assay = assayDict[newTrack.metadata.Assay] || ["unknown"];
             let sample = sampleDict[newTrack.metadata.Sample] || ["unknown"];
-            newTrack.datahub = hubMetadata.name;
+            newTrack.datahub = hubName;
             newTrack.metadata.Assay = assay.slice(0, -1);
             newTrack.metadata.assayDetails = assay[assay.length - 1];
             newTrack.metadata.Sample = sample.slice(0, -1);
