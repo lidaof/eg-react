0211524faa78aaf859b809809ebd6e1690e451cc
Daofeng Li <lidaof@gmail.com>
2018-10-03 14:48:52 -0500

    Trying adding long range track

---
+++ frontend/src/dataSources/bed/LongRange.worker.js
@@ -0,0 +1,4 @@
+import { LongRangeSourceWorker } from './LongRangeSourceWorker';
+import registerWorkerRunnableSource from '../worker/registerWorkerRunnableSource';
+
+registerWorkerRunnableSource(LongRangeSourceWorker);
---
+++ frontend/src/dataSources/bed/LongRangeSourceWorker.js
@@ -0,0 +1,23 @@
+import JSON5 from 'json5';
+import BedSourceWorker from './BedSourceWorker';
+import WorkerRunnableSource from '../worker/WorkerRunnableSource';
+
+export class LongRangeSourceWorker extends WorkerRunnableSource {
+    constructor(url) {
+        super();
+        this.bedSourceWorker = new BedSourceWorker(url, Infinity);
+    }
+
+    async getData(loci, basesPerPixel, options={}) {
+        const bedRecords = await this.bedSourceWorker.getData(loci);
+        bedRecords.map(record => {
+            let data = JSON5.parse('{' + record[3] + '}');
+            if (options.isRoughMode) {
+                data.genomealign.targetseq = null;
+                data.genomealign.queryseq = null;
+            }
+            record[3] = data;
+        });
+        return bedRecords;
+    }
+}
