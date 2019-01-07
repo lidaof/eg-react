abc434ab6018ad0d41e3e691dfb6145648229e7b
Daofeng Li <lidaof@gmail.com>
2019-01-07 10:07:39 -0600

    Update License

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 export class Footer extends React.Component {
     render (){
         return ( <div style={{marginTop: "5px", textAlign: "center", color: "gray"}}>
-            Copyright &copy; 2018 Washington University in St. Louis
+            Copyright &copy; 2018 Washington University in St. Louis. All rights reserved.
+            <br/>  Developed by the <a href="http://wang.wustl.edu" target="_blank">Wang Lab</a>
+<br/> <a href="LICENSE.html">Terms and Conditions of Use</a>
         </div>);
     }
 }
