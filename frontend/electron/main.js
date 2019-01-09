bba80e0b4c01622004edc5b84fb892b2b26295c4
Daofeng Li <lidaof@gmail.com>
2019-01-09 16:39:31 -0600

    Trying electron

---
+++ frontend/electron/main.js
@@ -0,0 +1,45 @@
+const {app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1200,
+    height: 900
+  })
+
+
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    win.loadURL(startUrl);
+
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
