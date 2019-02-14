a2a396a227d256992f7403dc85c58d32951756d6
D <d@ds-macbook-pro.attlocal.net>
2019-02-13 23:17:24 -0600

    Update hic-straw package for the normalization issue (see hic-straw issue #4 for details)

--- frontend/src/dataSources/HicSource.js
+++ frontend/src/dataSources/HicSource.js
@@ -111,7 +111,6 @@ export class HicSource extends DataSource {
         // if (normalization !== NormalizationMode.NONE) {
         //     await this.fetchNormalizationData();
         // }
-        console.log(this.normOptions, normalization);
         if (!this.normOptions.includes(normalization)) {
             return [];
         }
