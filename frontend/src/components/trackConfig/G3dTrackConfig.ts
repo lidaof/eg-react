0c7add326f02c71d4f482e08fbe01d595abad6a8
Daofeng Li <lidaof@gmail.com>
2019-11-10 23:55:01 -0600

    g3d data fetching on option change

--- frontend/src/components/trackConfig/G3dTrackConfig.ts
+++ frontend/src/components/trackConfig/G3dTrackConfig.ts
@@ -1,35 +1,56 @@
-import { TrackConfig } from './TrackConfig';
-import { G3dSource } from '../../dataSources/G3dSource';
-import G3dTrack, { DEFAULT_OPTIONS } from '../trackVis/3d/G3dTrack';
+import { TrackConfig } from "./TrackConfig";
+import { G3dSource } from "../../dataSources/G3dSource";
+import G3dTrack, { DEFAULT_OPTIONS } from "../trackVis/3d/G3dTrack";
 
-import HeightConfig from '../trackContextMenu/HeightConfig';
-import { BackgroundColorConfig } from 'components/trackContextMenu/ColorConfig';
-import TrackModel from '../../model/TrackModel';
-import { G3dResolutionConfig, G3dRegionConfig } from 'components/trackContextMenu/G3dDataConfig';
+import HeightConfig from "../trackContextMenu/HeightConfig";
+import { BackgroundColorConfig } from "components/trackContextMenu/ColorConfig";
+import TrackModel, { TrackOptions } from "../../model/TrackModel";
+import {
+  G3dResolutionConfig,
+  G3dRegionConfig
+} from "components/trackContextMenu/G3dDataConfig";
 
 export class G3dTrackConfig extends TrackConfig {
-    constructor(trackModel: TrackModel) {
-        super(trackModel);
-        this.setDefaultOptions({
-            ...DEFAULT_OPTIONS
-        });
-    }
+  constructor(trackModel: TrackModel) {
+    super(trackModel);
+    this.setDefaultOptions({
+      ...DEFAULT_OPTIONS
+    });
+  }
 
-    initDataSource() {
-        if (this.trackModel.fileObj) {
-            return new G3dSource(this.trackModel.fileObj);
-        } else {
-            return new G3dSource(this.trackModel.url);
-        }
+  initDataSource() {
+    if (this.trackModel.fileObj) {
+      return new G3dSource(this.trackModel.fileObj);
+    } else {
+      return new G3dSource(this.trackModel.url);
     }
+  }
 
-    getComponent() {
-        return G3dTrack;
-    }
+  /**
+   * @override
+   */
+  shouldFetchBecauseOptionChange(
+    oldOptions: TrackOptions,
+    newOptions: TrackOptions
+  ): boolean {
+    return (
+      oldOptions.region !== newOptions.region ||
+      oldOptions.resolution !== newOptions.resolution
+    );
+  }
 
-    getMenuComponents() {
-        const items = [G3dResolutionConfig, G3dRegionConfig, HeightConfig, BackgroundColorConfig];
+  getComponent() {
+    return G3dTrack;
+  }
 
-        return items;
-    }
+  getMenuComponents() {
+    const items = [
+      G3dResolutionConfig,
+      G3dRegionConfig,
+      HeightConfig,
+      BackgroundColorConfig
+    ];
+
+    return items;
+  }
 }
