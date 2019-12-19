d561dbdc155c81616b5d202a09741d9785a3333f
Daofeng Li <lidaof@gmail.com>
2019-12-18 23:26:11 -0600

    try PWA #2

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,43 +1,66 @@
+const { InjectManifest } = require("workbox-webpack-plugin");
+const path = require("path");
 
 module.exports = (config, env) => {
-    // This should help with typescript source-maps
-    config.devtool = 'source-map';
+  // This should help with typescript source-maps
+  config.devtool = "source-map";
 
-    if (!config.module) {
-        config.module = {};
+  if (!config.module) {
+    config.module = {};
+  }
+  if (!config.module.rules) {
+    config.module.rules = [];
+  }
+
+  config.module.rules.push({
+    loader: require.resolve("babel-loader"),
+    exclude: /node_modules/,
+    query: {
+      presets: [
+        "@babel/preset-typescript",
+        "@babel/preset-env",
+        "@babel/preset-react"
+      ],
+      plugins: [
+        "@babel/proposal-class-properties",
+        "@babel/proposal-object-rest-spread"
+      ]
+    },
+    test: /\.(js|jsx)$/
+  });
+
+  config.module.rules.push({
+    test: /\.worker\.js/,
+    use: {
+      loader: "worker-loader"
+      // options: { inline: true }
     }
-    if (!config.module.rules) {
-        config.module.rules = [];
-    }
+  });
 
-    config.module.rules.push({
-        loader: require.resolve('babel-loader'),
-        exclude: /node_modules/,
-        query: {
-          presets: [
-            "@babel/preset-typescript",
-            "@babel/preset-env",
-            "@babel/preset-react"
-        ],
-          plugins: [
-            "@babel/proposal-class-properties",
-            "@babel/proposal-object-rest-spread"
-        ]
-        },
-        test: /\.(js|jsx)$/,
-    });
+  config.output = {
+    ...config.output,
+    globalObject: "this"
+  };
 
-    config.module.rules.push({
-        test: /\.worker\.js/,
-        use: {
-            loader: 'worker-loader',
-            // options: { inline: true }
-        },       
-    });
-
-    config.output = {
-        ...config.output, globalObject: 'this'
+  if (env === "production") {
+    const workboxConfigProd = {
+      swSrc: path.join(__dirname, "public", "custom-service-worker.js"),
+      swDest: "custom-service-worker.js"
+      //   importWorkboxFrom: "disabled"
     };
-    
-    return config;
+    config = removeSWPrecachePlugin(config);
+    config.plugins.push(new InjectManifest(workboxConfigProd));
+  }
+
+  return config;
+};
+
+function removeSWPrecachePlugin(config) {
+  const swPrecachePluginIndex = config.plugins.findIndex(element => {
+    return element.constructor.name === "SWPrecacheWebpackPlugin";
+  });
+  if (swPrecachePluginIndex !== -1) {
+    config.plugins.splice(swPrecachePluginIndex, 1);
+  }
+  return config;
 }
---
+++ frontend/public/custom-service-worker.js
@@ -0,0 +1,14 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  /\.(?:js|css|html|png|ico)$/,
+  new workbox.strategies.NetworkFirst()
+);
