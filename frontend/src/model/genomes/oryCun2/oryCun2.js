26718500502fa96c1100f82445b8ed6436d698c8
xzhuo <yunkzo@gmail.com>
2021-03-01 12:29:48 -0600

    update rabbit genome annotation

--- frontend/src/model/genomes/oryCun2/oryCun2.js
+++ frontend/src/model/genomes/oryCun2/oryCun2.js
@@ -1,7 +1,7 @@
 import Chromosome from "../Chromosome";
 import Genome from "../Genome";
 import TrackModel from "../../TrackModel";
-import cytobands from "./cytoBand.json";
+import cytobands from "./cytoBandIdeo.json";
 import annotationTracks from "./annotationTracks.json";
 import chromSize from "./chromSize.json";
 const allSize = chromSize.map(genom => new Chromosome(genom.chr, genom.size));
@@ -11,17 +11,15 @@ const defaultRegion = navContext.parse("chr16:34702809-34709639");
 
 const defaultTracks = [
   new TrackModel({
+    type: "ruler",
+    name: "Ruler"
+  }),
+  new TrackModel({
     type: "refbed",
     name: "refGene",
     genome: "oryCun2",
     url: "https://wangftp.wustl.edu/~xzhuo/browser/oryCun2/oryCun2.refbed.gz"
   }),
-
-  new TrackModel({
-    type: "ruler",
-    name: "Ruler"
-  }),
-
   new TrackModel({
     type: "repeatmasker",
     name: "RepeatMasker",
