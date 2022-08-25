82dfa9278f92d507680f65c720520f02e6bdb6fb
Daofeng Li <lidaof@gmail.com>
2022-08-24 23:42:37 -0500

    adopting heng's code

--- frontend/src/components/trackVis/graphTrack/GraphTrack.tsx
+++ frontend/src/components/trackVis/graphTrack/GraphTrack.tsx
@@ -9,10 +9,12 @@ import OpenInterval from "model/interval/OpenInterval";
 import configOptionMerging from "../commonComponents/configOptionMerging";
 import Track from "../commonComponents/Track";
 import TrackLegend from "../commonComponents/TrackLegend";
+import { HiddenItemsMessage } from "../commonComponents/TrackMessage";
+import { GraphVisualizer } from "./GraphVisualizer";
 
 
 interface GraphTrackProps extends PropsFromTrackContainer, TooltipCallbacks {
-    data: Feature[];
+    data: any;
     options: {
         color?: string;
         color2?: string;
@@ -28,32 +30,18 @@ export const DEFAULT_OPTIONS = {
 const withDefaultOptions = configOptionMerging(DEFAULT_OPTIONS);
 
 /**
- * Track component for BED annotations.
+ * Track component for graph track.
  *
- * @author Silas Hsu
+ * @author Daofeng Li
  */
 class GraphTrackNoTooltip extends React.Component<GraphTrackProps> {
     static displayName = "GraphTrack";
 
     constructor(props: GraphTrackProps) {
         super(props);
-        this.renderTooltip = this.renderTooltip.bind(this);
     }
 
-    /**
-     * Renders the tooltip for a feature.
-     *
-     * @param {React.MouseEvent} event - mouse event that triggered the tooltip request
-     * @param {Feature} feature - Feature for which to display details
-     */
-    renderTooltip(event: React.MouseEvent, feature: Feature) {
-        const tooltip = (
-            <Tooltip pageX={event.pageX} pageY={event.pageY} onClose={this.props.onHideTooltip}>
-                <FeatureDetail feature={feature} />
-            </Tooltip>
-        );
-        this.props.onShowTooltip(tooltip);
-    }
+
 
     paddingFunc = (feature: Feature, xSpan: OpenInterval) => {
         const width = xSpan.end - xSpan.start;
@@ -78,8 +66,18 @@ class GraphTrackNoTooltip extends React.Component<GraphTrackProps> {
      * @returns 
      */
     render() {
-        const visualizer = <div>hello</div>
-        const message = <p>test</p>
+        const { data, visRegion, width, options } = this.props;
+        const height = 500//this.getHeight(arrangeResult.numRowsAssigned);
+        const visualizer = data.nodes ? <GraphVisualizer
+            data={data}
+            width={width}
+            height={height}
+            options={options}
+        /> : <div>loading...</div>
+        const message = <React.Fragment>
+            <HiddenItemsMessage numHidden={1} />
+            {'hello'}
+        </React.Fragment>;
         return (
             <Track
                 {...this.props}
